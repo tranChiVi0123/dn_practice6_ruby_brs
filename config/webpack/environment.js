e458589292c5f18d58851d455bc6d9be254e8411
tranChiVi0123 <idiots1230@gmail.com>
2021-02-02 11:09:45 +0700

    finish

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = required('webpack')
+environment.plugins.append('Provide',
+  new webpack.PovidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 module.exports = environment
